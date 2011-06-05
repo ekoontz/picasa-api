fdc421db35c1f590480d823c330833a730803e36
Eugene Koontz <ekoontz@hiro-tan.org>
2011-06-04 18:33:13 -0700

    sync with Steve

--- photos.php
+++ photos.php
@@ -10,9 +10,9 @@
 
 <style type="text/css">
 #contentWrap {
-	padding: 0px;
+padding: 0px;
 }
-div.tiles { 
+div.tiles {
 width:20%;
 float:left;
 display:block;
@@ -23,50 +23,69 @@ height:150px;
 </style>
 <script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-22082458-1']);
-  _gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22082458-1']);
+_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 </script>
+
+
+
+
+
+
+
+<link rel="image_src" href="http://qdobastreettacos.com/images/eventPhotos/031311/001t.jpg" />
+
+<script type="text/javascript">
+<!--
+function locPop1() {
+	window.open( "http://www.facebook.com/sharer.php?u=http://www.libraryschoolpdx.com/photos.php", "myWindow", "status = 1, height = 400, width = 600, resizable = 0" )
+}
+//-->
+</script>
+
+
 </head>
 
 <body>
 
 <div id="headerWrap">
 
-      <div id="header">
-          <div id="headerImage">
-              <a href="index.php"><img src="images/headerImage.png" alt="Qdoba Street Tacos" /></a>
-          </div>
-      </div>
+<div id="header">
+<div id="headerImage">
+<a href="index.php"><img src="images/headerImage.png" alt="Qdoba Street Tacos" /></a>
+</div>
+</div>
 </div>
 <div id="wrapper">
 
-	<div id="container">
-    
-        <div id="contentWrap">
+<div id="container">
+<div id="contentWrap">
 
-        
-            <div id="navWrap">
-	<div id="navigation">
-    	<a href="events.php"><img src="images/navEvent.png" alt="Event Photos" style="padding-right:30px;padding-left:90px;" /></a>
-        <a href="index.php"><img src="images/navHome.png" alt="Event Photos" /></a>
-    </div>            	
-</div><!-- NAV WRAP -->            
-           	<div id="pageWrap">
-            	<img src="images/pageHeaderEvent.png" alt="Event Photos" style="padding-bottom:0px;" /><br /><br />
+<div id="navWrap">
+<div id="navigation">
+<a href="events.php"><img src="images/navEvent.png" alt="Event Photos" style="padding-right:30px;padding-left:90px;" /></a>
+<a href="index.php"><img src="images/navHome.png" alt="Event Photos" /></a>
+</div>
+</div><!-- NAV WRAP -->
+<div id="pageWrap">
 
-                
+
+
+
+<img src="images/pageHeaderEvent.png" alt="Event Photos" style="padding-bottom:0px;" /><br /><br />
+<div align="center">
 <div class='bold' style='padding-bottom:20px;font-size:24px;'><?php echo $sxml->title; ?></div>
 
-<!--  smart back button -->
-<?php 
+<!-- smart back button -->
+<?php
 
 $albumid = urlencode($_GET["albumid"]);
 $userid = urlencode($_GET["userid"]);
@@ -80,6 +99,8 @@ echo "<a href=\"album.php?albumid=$albumid&userid=$userid\">Back to Photos</a>"
 Share your photo with your friends on Facebook you’ll instantly receive a coupon, redeemable at any Puget Sound area Qdoba location!</p>
 <br /><br />
 
+				<a href="coupon.php" onClick="locPop1()" style="font-weight:bold;"><img src="images/fbshare.jpg" alt="Share" /></a>
+				<br /><br />
 <?php
 
       
@@ -91,32 +112,29 @@ echo "<img src=\"$big\">";
 
     
     ?>
-                
-            </div>   
-            
     
-    	</div><!-- CONTENT WRAP -->
-        
-        <div id="footerBack" class="boneText">
-	<div id="footer">
+</div>
+</div>
+</div><!-- CONTENT WRAP -->
+<div id="footerBack" class="boneText">
+<div id="footer">
 
-        <div id="footerLeft">
-            2011 © <a href="http://qdoba.com" target="_blank">QDOBA MEXICAN GRILL</a>
-        </div>
-        <div id="footerCenter">
-            <a href="events.php">EVENT PHOTOS</a> | <a href="index.php">HOME</a> | <a href="pdf/officialRules.pdf" target="_blank">OFFICIAL RULES</a>
+<div id="footerLeft">
+2011 © <a href="http://qdoba.com" target="_blank">QDOBA MEXICAN GRILL</a>
+</div>
+<div id="footerCenter">
+<a href="events.php">EVENT PHOTOS</a> | <a href="index.php">HOME</a> | <a href="pdf/officialRules.pdf" target="_blank">OFFICIAL RULES</a>
 
-        </div>
-        <div id="footerRight">
-            <img src="images/footerLogo.png" alt="Qdoba" />
-        </div>
-    </div>
+</div>
+<div id="footerRight">
+<img src="images/footerLogo.png" alt="Qdoba" />
+</div>
+</div>
 </div>
 <br /><br />
 
 
-    
-    </div><!-- CONTAINER -->
+</div><!-- CONTAINER -->
 
 </div><!-- WRAPPER -->
 
